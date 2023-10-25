2da177d7a83050532e3db9a5ab6952f999e19b5d
John <shoemjoh@gmail.com>
2023-10-24 21:29:55 -0500

    find method lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+// Will loop through Super Bowl Results array
+// Find the first object whose condition returns true based
+// on a callback (cb) function (tester or finder)
+
+function superbowlWin(record) {
+    let win = record.find(object => object.result === 'W');
+    if (win) {
+        return win.year
+    }
+    else {
+        return undefined;
+    }
+}
